6e1f8602e6be2f719851042d4a43251d895c1726
Collin Meyer <collinmmeyer@gmail.com>
2022-02-12 13:20:30 -0600

    seperated into better file structure

---
+++ lchs/__init__.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_login import login_required
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from .content import CONTENT_FOLDER
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'TestingSecretKey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'login'
+    login_manager.init_app(app)
+
+    from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    app.config['CONTENT_FOLDER'] = CONTENT_FOLDER
+
+    app.view_functions['static'] = login_required(app.send_static_file)
+
+    from .auth import routes as auth_routes
+    app.register_blueprint(auth_routes.auth, url_prefix="/auth")
+
+    from .main import routes as main_routes
+    app.register_blueprint(main_routes.main, url_prefix="/")
+
+    return app
---
+++ lchs/content.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'heic', '.mp4'}
+
+CONTENT_FOLDER = os.getenv("CONTENT_FOLDER")
+
+if not CONTENT_FOLDER:
+    sys.exit("\u001b[31mCONTENT_FOLDER environmental variable must be set! Exiting...\u001b[0m")
+
+def checkVid(file: str) -> bool:
+    return file.endswith(".mp4")
+
+def checkImg(file: str) -> bool:
+    return file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png")
+
+def getContentList(t: str = None) -> str:
+    if t:
+        if t == "video":
+            return [x[0:-4] for x in os.listdir(CONTENT_FOLDER) if checkVid(x)]
+        elif t == "img":
+            return [x[0:-4] for x in os.listdir(CONTENT_FOLDER) if checkImg(x)]
+        else:
+            sys.exit(f'\u001b[31mType "{t}" passed to getContentList function is not valid\u001b[0m')
+    else:
+        return os.listdir(CONTENT_FOLDER)
