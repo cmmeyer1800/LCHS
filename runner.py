06e2b0b1c622f9bf9735e3644cd2a8f8d2a1c7a5
Collin Meyer <collinmmeyer@gmail.com>
2022-06-03 10:54:20 -0500

    initializing mac dev branch

--- runner.py
+++ runner.py
@@ -1,8 +1,8 @@
 import os
 
-os.environ["CONTENT_FOLDER"] = "/home/collin/Code/LCHS/lchs/content"
+os.environ["CONTENT_FOLDER"] = "/Users/collin/code/LCHS/lchs/content"
 
 from lchs import create_app
 
 app = create_app()
-app.run("0.0.0.0", port=5000, debug=True)
+app.run("0.0.0.0", port=4000, debug=True)
