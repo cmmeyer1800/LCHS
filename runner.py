411669dc59b9623ff22496e929e7eae776c5ffaa
Collin Meyer <collinmmeyer@gmail.com>
2022-06-03 12:52:42 -0500

    added logging

--- runner.py
+++ runner.py
@@ -1,6 +1,13 @@
-import os
+import sys
+
+if "--prod" in sys.argv:
+    prod = True
+    debug = False
+else:
+    prod = False
+    debug = True
 
 from lchs import create_app
 
-app = create_app()
-app.run("0.0.0.0", port=4000, debug=True)
+app = create_app(prod)
+app.run("0.0.0.0", port=4000, debug=debug)
