a39dc6ae341fba242cc67927b50b99a43757d9ee
Collin Meyer <collinmmeyer@gmail.com>
2022-06-09 20:27:24 -0500

    created real tests for LCHS

--- tests/test_status.py
+++ tests/test_status.py
@@ -1,9 +1,22 @@
-def test_http_response(client):
-    response = client.get('/main/')
-    assert response.status_code == 302
-    response = client.get('/main/live')
-    assert response.status_code == 302
-    response = client.get('/main/records')
-    assert response.status_code == 302
-    response = client.get('/main/issue')
-    assert response.status_code == 200
+def check_status_code_good(client, url):
+    response = client.get(url)
+    assert (response.status_code == 302 or response.status_code == 200)
+
+def check_status_code_bad(client, url):
+    response = client.get(url)
+    assert (response.status_code == 404)
+
+def test_index_response(client):
+    check_status_code_good(client, '/')
+
+def test_video_response(client):
+    check_status_code_good(client, '/video')
+
+def test_photo_response(client):
+    check_status_code_good(client, '/photo')
+
+def test_video_specific_response(client):
+    check_status_code_good(client, '/video/test_15s.mp4')
+
+def test_nonexistent_endpoint(client):
+    check_status_code_bad(client, "/badpath")
